bf9c61c5e58c7e99b22e0e2efe1e6b030cc7c195
musclemikiya <pawpuro10@gmail.com>
2016-12-30 01:23:14 +0900

    Initial Commit

---
+++ HouseWorkManager/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HouseWorkManager
+//
+//  Created by Mikiya Fukuda on 2016/12/30.
+//  Copyright © 2016年 Mikiya Fukuda. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
