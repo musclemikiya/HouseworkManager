b518e41184f05ca73394e3162c6e3e16f4577394
musclemikiya <pawpuro10@gmail.com>
2017-01-03 17:20:41 +0900

    Added play&pause&reset buttons

--- HouseWorkManager/Controllers/TimerViewController.swift
+++ HouseWorkManager/Controllers/TimerViewController.swift
@@ -11,10 +11,14 @@ import UIKit
 class TimerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var timerPickerView: UIPickerView!
+    @IBOutlet weak var minutesLabel: UILabel!
+    @IBOutlet weak var countDownLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
     
     let minutes: [Int] = ([Int])(1...59)
     var startSeconds: Int!
     var countDownTimer: Timer!
+    var isPaused = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,22 +40,47 @@ class TimerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return String(minutes[row])
     }
+    
+    private func reverseStatus() {
+        minutesLabel.isHidden = !minutesLabel.isHidden
+        timerPickerView.isHidden = !timerPickerView.isHidden
+        countDownLabel.isHidden = !countDownLabel.isHidden
+        startButton.isUserInteractionEnabled = !startButton.isUserInteractionEnabled
+    }
 
     @IBAction func startTimer(_ sender: Any) {
-        // TODO: pickerをカウントダウンに切り替え
+        // pickerとカウントダウンのlabelを切り替え
+        reverseStatus()
         startSeconds = minutes[timerPickerView.selectedRow(inComponent: 0)] * 60
         countDownTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (timer) in
+            if self.isPaused {
+                return
+            }
+            
             let minutes = String(abs(self.startSeconds) / 60)
             let seconds = String(format:"%02d" ,abs(self.startSeconds) % 60)
             
+            var labelString = ""
             if self.startSeconds < 0 {
-                print("- " + minutes + ":" + seconds)
+                labelString = "- " + minutes + ":" + seconds
             } else {
-                print(minutes + ":" + seconds)
+                labelString = minutes + ":" + seconds
             }
             
+            self.countDownLabel.text = labelString
             self.startSeconds = self.startSeconds - 1
         })
         countDownTimer.fire()
+    }
+    
+    @IBAction func pauseTimer(_ sender: Any) {
+        isPaused = !isPaused
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        if countDownTimer.isValid {
+            reverseStatus()
+            countDownTimer.invalidate()
+        }
     }
 }
